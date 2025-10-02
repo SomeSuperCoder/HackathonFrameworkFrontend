023c01f1d211297559bd22fea7d86c633dbadc92
SomeSuperCoder <ithelper9@gmail.com>
2025-10-02 20:10:26 +0300

    Basic tabs for participant

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,8 +1,26 @@
-import { UserContext } from "@/lib/context";
-import { useContext } from "react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+import MainPage from "./participant/MainPage";
+import MyTeam from "./participant/MyTeam";
+import Cases from "./participant/Cases";
 
 export default function Participant() {
-    const user = useContext(UserContext);
-
-    return <h1>Welcome, {user?.name}!</h1>;
+    return (
+        <Tabs defaultValue="main_page">
+            <TabsList className="w-screen">
+                <TabsTrigger value="main_page">Main page</TabsTrigger>
+                <TabsTrigger value="my_team">My Team</TabsTrigger>
+                <TabsTrigger value="cases">Cases</TabsTrigger>
+            </TabsList>
+            <TabsContent value="main_page">
+                <MainPage />
+            </TabsContent>
+            <TabsContent value="my_team">
+                <MyTeam />
+            </TabsContent>
+            <TabsContent value="cases">
+                <Cases />
+            </TabsContent>
+        </Tabs>
+    );
 }
---
+++ src/pages/participant/MyTeam.tsx
@@ -0,0 +1,3 @@
+export default function MyTeam() {
+    return <h1>My team</h1>;
+}
