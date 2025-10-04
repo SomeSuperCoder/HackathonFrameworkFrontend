63caa1d31cd82da3726f540a2c20728d382db0a3
SomeSuperCoder <ithelper9@gmail.com>
2025-10-04 23:31:27 +0300

    Leave team

--- src/lib/null_object_id.ts
+++ src/lib/null_object_id.ts
@@ -1,3 +1,3 @@
 import ObjectID from "bson-objectid";
 
-export const NullObjectID = ObjectID("000000000000000000000000");
+export const UndefinedObjectID = ObjectID("000000000000000000000001");
--- src/pages/participant/MyTeam.tsx
+++ src/pages/participant/MyTeam.tsx
@@ -1,11 +1,11 @@
 import PersonEntry from "@/components/PersonEntry";
 import { UserContext } from "@/lib/context";
-import { NullObjectID } from "@/lib/null_object_id";
+import { UndefinedObjectID } from "@/lib/null_object_id";
 import { useContext } from "react";
 import CreateTeamDialog from "@/components/CreateTeamDialog";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { ParseTeam, teamDriver } from "@/api/team";
-import { ParseUser } from "@/api/user";
+import { ParseUser, userDriver } from "@/api/user";
 import InfoAlert from "@/components/InfoAlert";
 import ErrorAlert from "@/components/ErrorAlert";
 import UploadSolution from "@/components/UploadSolution";
@@ -13,8 +13,12 @@ import EditTeamNameDialog from "@/components/EditTeamNameDialog";
 import { ButtonGroup } from "@/components/ui/button-group";
 import DeleteTeamDialog from "@/components/DeleteTeamDialog";
 import JoinTeam from "@/components/JoinTeam";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
 export default function MyTeam() {
+    const queryClient = useQueryClient();
+
     const user = useContext(UserContext)!;
 
     const {
@@ -37,7 +41,20 @@ export default function MyTeam() {
             ),
     });
 
-    if (user.team.equals(NullObjectID)) {
+    const leaveTeam = useMutation({
+        mutationFn: async () => {
+            await userDriver.updateUser(user._id, {
+                team: UndefinedObjectID.toHexString(),
+            });
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["user"],
+            });
+        },
+    });
+
+    if (user.team.equals(UndefinedObjectID)) {
         return (
             <div className="h-full flex flex-col justify-center items-center gap-2">
                 <h1 className="scroll-m-20 text-center sm:w-[70%] text-xl lg:text-2xl font-extrabold tracking-tight text-balance">
@@ -65,10 +82,20 @@ export default function MyTeam() {
         <div className="flex flex-col items-center mt-4 gap-4">
             <h1 className="flex gap-3 items-baseline scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
                 {team?.name}
-                <ButtonGroup>
-                    <EditTeamNameDialog team={team!} />
-                    <DeleteTeamDialog team={team!} />
-                </ButtonGroup>
+                {team?.leader.equals(user._id) ? (
+                    <ButtonGroup>
+                        <EditTeamNameDialog team={team!} />
+                        <DeleteTeamDialog team={team!} />
+                    </ButtonGroup>
+                ) : (
+                    <Button
+                        onClick={() => leaveTeam.mutate()}
+                        variant="destructive"
+                        size="icon"
+                    >
+                        <LogOut />
+                    </Button>
+                )}
             </h1>
             {team?.leader.equals(user._id) && <UploadSolution team={team} />}
             <div className="flex flex-col items-center gap-1">
